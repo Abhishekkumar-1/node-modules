8a24214eec90e796a6c89615c1f4c33cceba8d8d
Abhishekkumar-1 <abhisheksniperstar@gmail.com>
2024-01-02 00:59:43 +0530

    Worked on Express - All Static, API Vs SSR, JSON Basics, Params, Query String - Setup, Route Params, Params - Extra Info

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 const express=require('express')
-const path=require('path')
 const app=express()
 
-app.get('/',(req,res)=>{
-    res.status(200).sendFile(path.resolve(__dirname,'./index.html'))
+const {products}=require('./data')
+
+app.get('/author',(req,res)=>{
+    res.json([
+        {uid:'20BCS9801',name:'Abhishek kumar',age:19},
+        {uid:'NA',name:'NA',age:21}
+    ])
 })
 
-app.all('*',(req,res)=>{
-    res.status(404).send('Resource not found')
+app.get('/',(req,res)=>{
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products/allproducts',(req,res)=>{
+    const newProducts=products.map((product)=>{
+        const {id,name,image}=product
+        return {id,name,image}
+    })
+
+    res.json(newProducts)
+})
+
+app.get('/api/products/:productID',(req,res)=>{
+    // console.log(req.params)
+    const {productID}=req.params
+    if(isNaN(productID)){
+        return res.send('<h3>Product id is should be integer</h3>')
+    }
+    const singleProduct=products.find((product)=>product.id===Number(productID))
+    
+    if(!singleProduct){
+        return res.status(404).send('<h3>Not found product</h3>')
+    }
+    
+    return res.json(singleProduct)
+    
 })
 
 app.listen(5000,()=>{
-    console.log('Server sleeping on port 5000.....')
+    console.log('Server is listening  on 5000...')
 })
 
-//5:07
+// 5:50
