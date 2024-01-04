c88e01de1bd2c3b3c44ae06aa8f6ea67b7939930
Abhishekkumar-1 <abhisheksniperstar@gmail.com>
2024-01-04 22:04:46 +0530

    express methods,middleware

--- app.js
+++ app.js
@@ -5,9 +5,52 @@ let {people}=require('./data')
 
 app.use(express.static('./methods-public'))
 app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 
 app.get('/api/people',(req,res)=>{
     res.status(200).json({success:true,data:people})
+})
+
+app.post('/api/people',(req,res)=>{
+    const {name}=req.body
+    if(!name){
+        return res.status(400).json({success:false,msg:'Please provide a name'})
+    }
+    res.status(201).json({success:true,person:name})
+})
+
+app.put('/api/people/:id',(req,res)=>{
+    const {id}=req.params
+    const {name}=req.body
+    // console.log(id,name)
+    // res.send('hello world!')
+    const person=people.find((person)=>person.id===Number(id))
+
+    if(!person){
+        return res.status(400).json({success:false,msg:`no person with id ${id}`})
+    }
+    const newPeople=people.map((person)=>{
+        if(person.id===Number(id)){
+            person.name=name
+        }
+        return person
+    })
+
+    res.status(200).json({success:true,data:newPeople})
+})
+
+app.delete('/api/people/:id',(req,res)=>{
+    const {id}=req.params
+    const {name}=req.body
+
+    const person=people.find((person)=>person.id===Number(id))
+    if(!person){
+        return res.status(200).json({success:true,msg:`No person with id ${id}`})
+    }
+
+    const newPeople=people.filter((person)=>person.id!==Number(id))
+
+    res.status(200).json({success:true,data:newPeople})
 })
 
 app.post('/login',(req,res)=>{
--- data.js
+++ data.js
@@ -37,6 +37,6 @@ const products = [
     { id: 2, name: 'peter' },
     { id: 3, name: 'susan' },
     { id: 4, name: 'anna' },
-    { id: 5, name: 'emma' },
+    { id: 5, name: 'kumar' },
   ]
   module.exports = { products, people }
